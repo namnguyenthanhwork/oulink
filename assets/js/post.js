bf263779d7ca418f297b7629d10f5f0537c1dd9d
namnguyenthanhwork <78526193+namnguyenthanhwork@users.noreply.github.com>
2021-11-14 17:59:30 +0700

    update

--- assets/js/post.js
+++ assets/js/post.js
@@ -18,7 +18,7 @@ let post = [{
     },
     {
         author: 'Diệp Huy',
-        image: 'assets/images/postGr/post-6.jpg',
+        image: 'assets/images/postGr/post-3.jpg',
         image_hot: 'assets/images/postGr/new.gif',
         link: 'https://www.facebook.com/groups/oumembers/posts/1351872075229397/',
         time_delay: '0.8s',
@@ -27,81 +27,357 @@ let post = [{
     },
     {
         author: 'Thành Nam',
-        image: 'assets/images/postGr/post-3.jpg',
+        image: 'assets/images/postGr/post-4.jpg',
         image_hot: '',
         link: 'https://www.facebook.com/groups/oumembers/posts/1347522502331021/',
-        time_delay: '0.8s',
+        time_delay: '0.2s',
         time: '18/10/2021',
         title: 'Hướng dẫn sửa lỗi font slide của giảng viên'
     },
     {
         author: 'Thuong Dong',
-        image: 'assets/images/postGr/post-4.jpg',
+        image: 'assets/images/postGr/post-5.jpg',
         image_hot: '',
         link: 'https://www.facebook.com/groups/oumembers/posts/1345719595844645/',
-        time_delay: '0.2s',
+        time_delay: '0.5s',
         time: '15/10/2021',
         title: 'Thông báo về việc mua tài liệu học tập'
     },
     {
         author: 'Minh Long',
-        image: 'assets/images/postGr/post-5.jpg',
+        image: 'assets/images/postGr/post-6.jpg',
         image_hot: '',
         link: 'https://www.facebook.com/groups/oumembers/posts/1345650342518237/',
-        time_delay: '0.5s',
+        time_delay: '0.8s',
         time: '15/10/2021',
         title: 'TRUNG TÂM HƯỚNG NGHIỆP - TƯ VẤN VIỆC LÀM TRƯỜNG ĐH MỞ TP.HCM'
+    },
+    {
+        author: 'Thảo Vy Ngo',
+        image: 'assets/images/postGr/post-7.jpg',
+        link: 'https://www.facebook.com/groups/oumembers/posts/1343930692690202/',
+        time_delay: '0.2s',
+        time: '13/10/2021',
+        title: 'CANVA FOR EDUCATION - XÀI FREE NHỜ CÓ MAIL TRƯỜNG'
+    },
+    {
+        author: 'Thành Nam',
+        image: 'assets/images/postGr/post-8.png',
+        link: 'https://www.facebook.com/groups/oumembers/posts/1338495006567104/',
+        time_delay: '0.5s',
+        time: '05/10/2021',
+        title: 'Thông báo mở LMS 211'
+    },
+    {
+        author: 'Thảo Vy Ngo',
+        image: 'assets/images/postGr/post-9.jpg',
+        link: 'https://www.facebook.com/groups/oumembers/posts/1337789979970940/',
+        time_delay: '0.8s',
+        time: '04/10/2021',
+        title: 'NOTION PERSONAL PRO - XÀI FREE NHỜ CÓ MAIL TRƯỜNG'
+    },
+    {
+        author: 'Diệu Nhân',
+        image: 'assets/images/postGr/post-10.jpg',
+        link: 'https://www.facebook.com/groups/oumembers/posts/1335854933497778/',
+        time_delay: '0.2s',
+        time: '01/10/2021',
+        title: 'MỘT SỐ KINH NGHIỆM ĐẠT HỌC BỔNG KHUYẾN KHÍCH HỌC TẬP'
+    },
+    {
+        author: 'Quang Hà',
+        image: 'assets/images/postGr/post-11.jpg',
+        link: 'https://www.facebook.com/groups/oumembers/posts/1334417803641491/',
+        time_delay: '0.5s',
+        time: '29/09/2021',
+        title: 'Thông báo Vv tổ chức kiểm tra năng lực đầu vào dành cho Tân sinh viên đã đăng ký kiểm tra theo các phương thức trực tiếp'
+    },
+    {
+        author: 'Quang Hà',
+        image: 'assets/images/postGr/post-12.jpg',
+        link: 'https://www.facebook.com/groups/oumembers/posts/1329863790763559/',
+        time_delay: '0.8s',
+        time: '22/09/2021',
+        title: 'Đơn gia hạn đóng học phí k21'
+    },
+    {
+        author: 'Bích Trâm',
+        image: 'assets/images/postGr/post-13.jpg',
+        link: 'https://www.facebook.com/groups/oumembers/posts/1329794394103832/',
+        time_delay: '0.2s',
+        time: '22/09/2021',
+        title: 'HƯỚNG DẪN IN GIẤY BÁO TRÚNG TUYỂN TRỰC TUYẾN'
+    },
+    {
+        author: 'Đình Phương',
+        image: 'assets/images/postGr/post-14.jpg',
+        link: 'https://www.facebook.com/groups/oumembers/posts/1326181374465134/',
+        time_delay: '0.5s',
+        time: '17/09/2021',
+        title: 'THÔNG BÁO v/v xác nhận NHẬP HỌC thí sinh trúng tuyển PT kết quả thi TN THPT và ĐẶC CÁCH xét tốt nghiệp'
+    },
+    {
+        author: 'Thành Nam',
+        image: 'assets/images/postGr/post-15.jpg',
+        link: 'https://www.facebook.com/groups/oumembers/posts/1325308404552431/',
+        time_delay: '0.8s',
+        time: '16/09/2021',
+        title: 'Thông báo điểm chuẩn trúng tuyển kỳ tuyển sinh ĐHCQ năm 2021'
+    },
+    {
+        author: 'Thành Nam',
+        image: 'assets/images/postGr/post-16.jpg',
+        link: 'https://www.facebook.com/groups/oumembers/posts/1322102611539677/',
+        time_delay: '0.2s',
+        time: '11/09/2021',
+        title: 'Thông báo (BỔ SUNG) Về việc tổ chức kiểm tra trình độ tiếng Anh - Tin học đầu vào cho Tân sinh viên và vấn đề liên quan đến tài khoản - khóa 2021'
+    },
+    {
+        author: 'Quang Hà',
+        image: 'assets/images/postGr/post-17.jpg',
+        link: 'https://www.facebook.com/groups/oumembers/posts/1321268808289724/',
+        time_delay: '0.5s',
+        time: '10/09/2021',
+        title: 'HƯỚNG DẪN Tân sinh viên khóa 2021 sử dụng các tài khoản được cấp và tham gia kiểm tra trình độ đầu vào (Tiếng Anh và Tin học)'
+    },
+    {
+        author: 'Thành Nam',
+        image: 'assets/images/postGr/post-1.jpg',
+        link: 'https://www.facebook.com/groups/oumembers/posts/1320830008333604/',
+        time_delay: '0.8s',
+        time: '09/09/2021',
+        title: 'Chatbot ôn luyện tin học và anh văn đầu vào'
+    },
+    {
+        author: 'Quang Hà',
+        image: 'assets/images/postGr/post-2.jpg',
+        link: 'https://www.facebook.com/groups/oumembers/posts/1320479661701972/',
+        time_delay: '0.2s',
+        time: '09/09/2021',
+        title: 'Thông báo vv cập nhật học phí và kiểm tra hồ sơ trên web'
+    },
+    {
+        author: 'Thành Nam',
+        image: 'assets/images/postGr/post-3.jpg',
+        link: 'https://www.facebook.com/groups/oumembers/posts/1312833805799891/',
+        time_delay: '0.5s',
+        time: '29/08/2021',
+        title: 'THÔNG BÁO VỀ VIỆC TỔ CHỨC KIỂM TRA TRÌNH ĐỘ TIẾNG ANH - TIN HỌC ĐẦU VÀO CHO TÂN SINH VIÊN KHÓA 2021'
+    },
+    {
+        author: 'Thành Nam',
+        image: 'assets/images/postGr/post-4.jpg',
+        link: 'https://www.facebook.com/groups/oumembers/posts/1312469992502939/',
+        time_delay: '0.8s',
+        time: '28/08/2021',
+        title: 'Thời khoá biểu khoá k21'
+    },
+    {
+        author: 'Thành Nam',
+        image: 'assets/images/postGr/post-5.jpg',
+        link: 'http://tuyensinh.ou.edu.vn/thong-tin-xet-tuyen-phuong-thuc-diem-thi-tn-thpt-2021',
+        time_delay: '0.2s',
+        time: '28/08/2021',
+        title: 'Thông tin xét tuyển phương thức điểm thi TN THPT 2021'
+    },
+    {
+        author: 'Thành Nam',
+        image: 'assets/images/postGr/post-6.jpg',
+        link: 'https://www.facebook.com/groups/oumembers/posts/1310730402676898/',
+        time_delay: '0.5s',
+        time: '26/08/2021',
+        title: 'ĐĂNG KÝ XÉT TUYỂN ĐẠI HỌC (dành cho xét đợt 1 và đặc cách, đã đăng ký nhập học bạ trước đó).'
+    },
+    {
+        author: 'Thành Nam',
+        image: 'assets/images/postGr/post-7.jpg',
+        link: 'https://www.facebook.com/groups/oumembers/posts/1309858822764056/',
+        time_delay: '0.8s',
+        time: '25/08/2021',
+        title: 'ĐĂNG KÝ XÉT TUYỂN ĐẠI HỌC (dành cho xét đặc cách và chưa đăng ký đợt 1).'
+    },
+    {
+        author: 'Thành Nam',
+        image: 'assets/images/postGr/post-8.png',
+        link: 'https://www.facebook.com/groups/oumembers/posts/1309864012763537/',
+        time_delay: '0.2s',
+        time: '25/08/2021',
+        title: 'Hướng dẫn v/v Nộp Giấy CNKQ Tốt nghiệp THPT 2021 trễ hạn.'
+    },
+    {
+        author: 'Thành Nam',
+        image: 'assets/images/postGr/post-9.jpg',
+        link: '"https://www.facebook.com/groups/oumembers/posts/1305405273209411/',
+        time_delay: '0.5s',
+        time: '18/08/2021',
+        title: 'Lưu ý khi viết mail và mẫu đơn gửi cho ban tuyển sinh.'
+    },
+    {
+        author: 'Thành Nam',
+        image: 'assets/images/postGr/post-10.jpg',
+        link: 'https://www.facebook.com/groups/oumembers/posts/1305343249882280/',
+        time_delay: '0.8s',
+        time: '18/08/2021',
+        title: 'Thông báo gia hạn nộp hồ sơ đợt 1, xét đợt 2 và đặc cách.'
+    },
+    {
+        author: 'Đình Phương',
+        image: 'assets/images/postGr/post-11.jpg',
+        link: 'https://www.facebook.com/groups/oumembers/posts/1303907133359225/',
+        time_delay: '0.2s',
+        time: '16/08/2021',
+        title: 'Điểm sàn thi THPTQG Đợt 1 năm 2021'
+    },
+    {
+        author: 'Thành Nam',
+        image: 'assets/images/postGr/post-12.jpg',
+        link: 'https://www.facebook.com/groups/oumembers/posts/1303932290023376/',
+        time_delay: '0.5s',
+        time: '16/08/2021',
+        title: 'Thông báo tuyển sinh thí sinh đặc cách xét công nhận tốt nghiệp THPT năm 2021.'
+    },
+    {
+        author: 'Thành Nam',
+        image: 'assets/images/postGr/post-13.jpg',
+        link: 'https://www.facebook.com/groups/oumembers/posts/1301559593593979/',
+        time_delay: '0.8s',
+        time: '12/08/2021',
+        title: 'Giới thiệu về Hệ thống điểm tích lũy 4 năm đại học.'
+    },
+    {
+        author: 'Thành Nam',
+        image: 'assets/images/postGr/post-14.jpg',
+        link: 'https://www.facebook.com/groups/oumembers/posts/1299457123804226/',
+        time_delay: '0.2s',
+        time: '09/08/2021',
+        title: 'Giới thiệu Hệ thống điểm rèn luyện của trường Đại học Mở.'
+    },
+    {
+        author: 'Quang Hà',
+        image: 'assets/images/postGr/post-15.jpg',
+        link: 'https://www.facebook.com/groups/oumembers/posts/1293426214407317/',
+        time_delay: '0.5s',
+        time: '31/07/2021',
+        title: 'Cảm nhận về OU.'
+    },
+    {
+        author: 'Thành Nam',
+        image: 'assets/images/postGr/post-16.jpg',
+        link: 'https://www.facebook.com/groups/oumembers/posts/1292582841158321/',
+        time_delay: '0.8s',
+        time: '30/07/2021',
+        title: 'Các giấy tờ đi kèm trong hồ sơ nộp về Trường.'
+    },
+    {
+        author: 'Thành Nam',
+        image: 'assets/images/postGr/post-17.jpg',
+        link: 'https://www.facebook.com/groups/oumembers/posts/1291496771266928/',
+        time_delay: '0.2s',
+        time: '28/07/2021',
+        title: 'Làm thế nào để biết được hồ sơ sẽ đến trường '
+    },
+    {
+        author: 'Thành Nam',
+        image: 'assets/images/postGr/post-1.jpg',
+        link: 'https://www.facebook.com/groups/oumembers/posts/1290067958076476/',
+        time_delay: '0.5s',
+        time: '26/07/2021',
+        title: 'Timeline xét tuyển thi thptqg đợt 1, công cụ phân tích điểm thi 2021.'
+    },
+    {
+        author: 'Hieu Q.Nguyen',
+        image: 'assets/images/postGr/post-2.jpg',
+        link: 'https://www.facebook.com/groups/oumembers/posts/1263937307356208/',
+        time_delay: '0.8s',
+        time: '17/06/2021',
+        title: 'Ngành ngôn ngữ anh tại đại học Mở tp.HCM.'
+    },
+    {
+        author: 'Quang Hà',
+        image: 'assets/images/postGr/post-3.jpg',
+        link: 'https://www.facebook.com/groups/oumembers/posts/1291199234630015/',
+        time_delay: '0.2s',
+        time: '28/07/2021',
+        title: 'Góc nhìn về đại học Mở từ 1 sv năm nhất.'
+    },
+    {
+        author: '',
+        image: 'assets/images/postGr/post-13.jpg',
+        link: '',
+        time_delay: '0.2s',
+        time: '05/10/2021',
+        title: ''
+    },
+    {
+        author: '',
+        image: 'assets/images/postGr/post-13.jpg',
+        link: '',
+        time_delay: '0.2s',
+        time: '05/10/2021',
+        title: ''
+    },
+    {
+        author: '',
+        image: 'assets/images/postGr/post-13.jpg',
+        link: '',
+        time_delay: '0.2s',
+        time: '05/10/2021',
+        title: ''
+    },
+    {
+        author: '',
+        image: 'assets/images/postGr/post-13.jpg',
+        link: '',
+        time_delay: '0.2s',
+        time: '05/10/2021',
+        title: ''
+    },
+    {
+        author: '',
+        image: 'assets/images/postGr/post-13.jpg',
+        link: '',
+        time_delay: '0.2s',
+        time: '05/10/2021',
+        title: ''
+    },
+    {
+        author: '',
+        image: 'assets/images/postGr/post-13.jpg',
+        link: '',
+        time_delay: '0.2s',
+        time: '05/10/2021',
+        title: ''
+    },
+    {
+        author: '',
+        image: 'assets/images/postGr/post-13.jpg',
+        link: '',
+        time_delay: '0.2s',
+        time: '05/10/2021',
+        title: ''
+    },
+    {
+        author: '',
+        image: 'assets/images/postGr/post-13.jpg',
+        link: '',
+        time_delay: '0.2s',
+        time: '05/10/2021',
+        title: ''
+    },
+    {
+        author: '',
+        image: 'assets/images/postGr/post-13.jpg',
+        link: '',
+        time_delay: '0.2s',
+        time: '05/10/2021',
+        title: ''
+    },
+    {
+        author: '',
+        image: 'assets/images/postGr/post-13.jpg',
+        link: '',
+        time_delay: '0.2s',
+        time: '05/10/2021',
+        title: ''
     }
 ]
-
-let post_list = document.querySelector('#group-post');
-
-renderPost = (post) => {
-    post.forEach(function (e, i) {
-        switch (i) {
-            case 0:
-            case 3: {
-                e.time_delay = "0.2s"
-                break
-            }
-            case 1:
-            case 4: {
-                e.time_delay = "0.5s"
-                break
-            }
-            case 2:
-            case 5: {
-                e.time_delay = "0.8s"
-                break
-            }
-        }
-        let prod =
-            `
-            <div class="df col-lg-4 col-md-6 col-sm-9">
-                <div class="single-blog mt-30 wow fadeIn" data-wow-duration="1s" data-wow-delay="${e.time_delay}">
-                    <div class="blog-image">
-                        <img src="${e.image}" alt="blog">       
-            `
-
-        if (!e.image_hot == '')
-            prod += `<img src="${e.image_hot}" alt="new">`
-
-        prod +=
-            ` </div>
-                <div class="blog-content">
-                    <ul class="meta">
-                        <li><b>Đăng bởi:</b> <a href="#">${e.author}</a></li>
-                        <li>${e.time}</li>
-                    </ul>
-                    <p class="text">${e.title}</p>
-                    <a class="more" href="${e.link}"
-                        target="_blank">Xem
-                        chi tiết<i class="lni-chevron-right"></i></a>
-                </div>
-            </div> <!-- single blog -->
-        </div>
-        `
-        post_list.insertAdjacentHTML("beforeend", prod)
-    })
-}
-renderPost(post);
